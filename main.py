cb0d86b4f40ded8f2b179d7375571bb0aeac0032
luise <luiserivaldo99@gmail.com>
2022-03-12 04:21:54 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import sys
+import random
+
+
+def main():
+    user_length = input(print("Insert the length of the password: "))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
